b6c57ebe88097af753b33d6e72e5f0b63304b84e
miguelangel.rendon@accenture.com <miguelangel rendon>
2021-08-31 14:49:08 -0500

    Implement logger.

--- app/services/weets.js
+++ app/services/weets.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const { weetApi } = require('../../config').common.api;
 const { weetApiError } = require('../errors');
+const logger = require('../logger');
 const ErrorMessages = require('../../config/error');
 
 exports.getWeet = async () => {
@@ -8,6 +9,7 @@ exports.getWeet = async () => {
     const { data } = await axios.get(weetApi);
     return data;
   } catch (error) {
+    logger.error(error.message);
     return weetApiError(ErrorMessages.WEET_API_ERROR);
   }
 };
