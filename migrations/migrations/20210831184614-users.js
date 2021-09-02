cd2efd802d198a56c087d2f91ce1798d2011e240
miguelangel.rendon@accenture.com <miguelangel rendon>
2021-09-02 10:07:06 -0500

    remove properti.

--- migrations/migrations/20210831184614-users.js
+++ migrations/migrations/20210831184614-users.js
@@ -2,21 +2,15 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      'users',
-      {
-        id: { type: Sequelize.INTEGER, unique: true, autoIncrement: true, allowNull: false },
-        name: { type: Sequelize.STRING, allowNull: false },
-        last_name: { type: Sequelize.STRING, allowNull: false },
-        email: { type: Sequelize.STRING, unique: true, allowNull: false },
-        password: { type: Sequelize.STRING, allowNull: false },
-        created_at: { type: Sequelize.DATE },
-        updated_at: { type: Sequelize.DATE }
-      },
-      {
-        underscored: true
-      }
-    );
+    await queryInterface.createTable('users', {
+      id: { type: Sequelize.INTEGER, unique: true, autoIncrement: true, allowNull: false },
+      name: { type: Sequelize.STRING, allowNull: false },
+      last_name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, unique: true, allowNull: false },
+      password: { type: Sequelize.STRING, allowNull: false },
+      created_at: { type: Sequelize.DATE },
+      updated_at: { type: Sequelize.DATE }
+    });
   },
 
   down: async queryInterface => {
