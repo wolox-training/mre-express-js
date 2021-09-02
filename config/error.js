2065a28135ac4b5fc5472b06528071814ad43b99
miguelangel.rendon@accenture.com <miguelangel rendon>
2021-09-02 16:12:31 -0500

    new error by users

--- config/error.js
+++ config/error.js
@@ -1,5 +1,10 @@
 const ErrorMessages = {
-  WEET_API_ERROR: 'Weet api request is failed'
+  // Weet Api
+  WEET_API_ERROR: 'Weet api request is failed',
+
+  // User
+  USER_EMAIL_NOT_COMPANY: 'The email is not pertain to company.',
+  USER_EMAIL_EXITS: 'This email already exists'
 };
 
 module.exports = ErrorMessages;
