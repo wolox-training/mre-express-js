ee3aadae2c1e75c429842c1f54cdde3a15f52f02
miguelangel.rendon@accenture.com <miguelangel rendon>
2021-09-06 10:37:46 -0500

    Character and return response.

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,27 +1,26 @@
 const EMAIL_MACH = 'wolox.co';
-const ALPHANUMERIC = /^[a-zA-Z0-9]+$/;
-const MAX_CHARACTERS = 8;
+const ALPHANUMERIC = /^[a-zA-Z0-9]{8,}$/;
 const { User } = require('../models/index');
 const logger = require('../logger');
 const ErrorMessages = require('../../config/error');
 
-exports.createUser = async (req, res) => {
+exports.createUser = async (req, res, next) => {
   const { name, lastName, email, password } = req.body;
-  if (!ALPHANUMERIC.test(password) && password.length >= MAX_CHARACTERS) {
+  if (!ALPHANUMERIC.test(password)) {
     logger.error(ErrorMessages.PASSWORD_NOT_COMPLY);
-    res.status(400).json(ErrorMessages.PASSWORD_NOT_COMPLY);
+    return res.status(400).json(ErrorMessages.PASSWORD_NOT_COMPLY);
   }
 
   const validateEmail = email.split('@', 2);
   if (EMAIL_MACH !== validateEmail[1]) {
     logger.error(ErrorMessages.USER_EMAIL_NOT_COMPANY);
-    res.status(400).json({ message: ErrorMessages.USER_EMAIL_NOT_COMPANY });
+    return res.status(400).json({ message: ErrorMessages.USER_EMAIL_NOT_COMPANY });
   }
 
   const exitEmail = await User.findOne({ where: { email } });
   if (exitEmail) {
     logger.error(ErrorMessages.USER_EMAIL_EXITS);
-    res.status(400).json({ message: ErrorMessages.USER_EMAIL_EXITS });
+    return res.status(400).json({ message: ErrorMessages.USER_EMAIL_EXITS });
   }
   const user = await User.create({
     name,
@@ -30,5 +29,5 @@ exports.createUser = async (req, res) => {
     password
   });
 
-  res.status(201).json(user.name);
+  return res.status(201).json(user.name);
 };
