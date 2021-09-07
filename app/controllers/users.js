661ccbfa82caac7646c0f80eb1c371d915046341
miguelangel.rendon@accenture.com <miguelangel rendon>
2021-09-07 14:43:18 -0500

    Password

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,7 @@
 const logger = require('../logger');
 const { createUser } = require('../services/users');
 const { validatePassword, validateEmail, existEmail } = require('../validations/users');
+const { encryptPassword } = require('../helpers/password');
 
 exports.createUser = async (req, res, next) => {
   try {
@@ -9,7 +10,8 @@ exports.createUser = async (req, res, next) => {
     validateEmail(email);
     validatePassword(password);
     await existEmail(email);
-    const user = await createUser(name, lastName, email, password);
+    const newPassword = await encryptPassword(password);
+    const user = await createUser({ name, lastName, email, password: newPassword });
     logger.info(`The ${email} is created successfull`);
     return res.status(201).json(user.name);
   } catch (error) {
--- app/helpers/password.js
+++ app/helpers/password.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcryptjs');
+const { constants } = require('../../config/constants');
 
 exports.encryptPassword = async password => {
-  const salt = await bcrypt.genSaltSync(8, 'a');
-  return bcrypt.hashSync(password, salt);
+  const salt = await bcrypt.genSaltSync(constants.LENGTH_PASSWORD_ENCRYPT);
+  const encryptPassword = bcrypt.hashSync(password, salt);
+  return encryptPassword;
 };
 
 exports.validPassword = (password, comparePassword) => bcrypt.compare(password, comparePassword);
